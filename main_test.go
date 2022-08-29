55d5b96577a1f1113d16f4305fe3a3e8c34c9e30
tchiunam <tchiunam@users.noreply.github.com>
2022-08-29 21:38:49 +0800

    Add command main.go automated test

--- main_test.go
+++ main_test.go
@@ -19,15 +19,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
 package main
 
-import "github.com/tchiunam/axolgo-cli/pkg/cmd"
+import (
+	"testing"
 
-func main() {
-	cmd.Execute()
-}
+	"github.com/stretchr/testify/assert"
+)
 
-// A wrapper for automated testing
-func MainWrapper() {
-	main()
+func TestMain(t *testing.T) {
+	t.Run("call main", func(t *testing.T) {
+		assert.NotPanics(t, MainWrapper)
+	})
 }
